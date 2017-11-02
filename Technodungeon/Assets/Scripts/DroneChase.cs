171f91b120c597c62e9d3417bae76adf59dbf020
slobanwala1 <slobanwala@gmail.com>
2017-11-02 13:12:48 -0500

    Added a chase class for drone.

--- Technodungeon/Assets/Scripts/DroneChase.cs
+++ Technodungeon/Assets/Scripts/DroneChase.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DroneChase : MonoBehaviour {
-
+    public Transform player;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +11,16 @@ public class DroneChase : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Vector3.Distance(player.position, this.transform.position) < 10)
+        {
+            Vector3 direction = player.position - this.transform.position;
+            direction.y = 0;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1F);
+            
+            if(direction.magnitude > 5)
+            {
+                this.transform.Translate(0, 0, 0.05F);
+            }
+        }
 	}
 }
