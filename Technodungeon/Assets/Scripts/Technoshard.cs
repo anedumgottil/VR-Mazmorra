a20e782f7d61caa1c11869f2f99a658a0cdaf6da
Andrew Podgorski <apodgo2@uic.edu>
2017-11-09 13:54:37 -0600

    Added Technoshard.cs

---
+++ Technodungeon/Assets/Scripts/Technoshard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO: set this to stationary...
+public class Technoshard : MobileEntity {
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Contains ("Controller")) {
+            Destroy (this.gameObject);
+        }
+        //SetCollidingObject(other);
+    }
+}
