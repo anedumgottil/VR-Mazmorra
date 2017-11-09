4e21e0e23a0793ce277f2a7921b87025769ee7aa
Anthony Nedumgottil <anedum2@uic.edu>
2017-11-09 13:40:27 -0600

    added prefabs for bullet

---
+++ Technodungeon/Assets/onhit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://stackoverflow.com/questions/30280187/emit-particle-oncollision-in-unity-3d
+
+public class onhit : MonoBehaviour {
+	public ParticleSystem collisionParticlePrefab; //Assign the Particle from the Editor (You can do this from code too)
+	private ParticleSystem tempCollisionParticle;
+
+	void OnCollisionEnter(Collider other)
+	{
+		if(other.tag == "turret")
+		{
+			tempCollisionParticle = Instantiate (collisionParticlePrefab, transform.position, Quaternion.identity) as ParticleSystem;
+			tempCollisionParticle.Play ();
+		}
+
+	}
+}
