1a400cb2cdfc65350905f3d63ab176a89b6775bc
slobanwala1 <slobanwala@gmail.com>
2017-11-16 11:36:38 -0600

    Main menu added
    Added main menu with exit button.

---
+++ Technodungeon/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void newGameBTN(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+    public void exitGameBTN()
+    {
+        Application.Quit();
+    }
+}
