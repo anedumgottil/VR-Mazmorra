3f3678c7fcfbba54cdfbc30597c6f02821a9cfb9
slobanwala1 <slobanwala@gmail.com>
2017-11-16 12:04:10 -0600

    Added sound for Main menu and others
    Added main menu music and gem music

--- Technodungeon/Assets/Scripts/Technoshard.cs
+++ Technodungeon/Assets/Scripts/Technoshard.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 //TODO: set this to stationary...
 public class Technoshard : StationaryEntity {
-
+    public AudioSource gemNoise;
     public Technoshard(GridSpace gs) : base(gs) {
         //new Technoshard
+        gemNoise = GetComponent<AudioSource>();
     }
 
     public void OnTriggerEnter(Collider other)
     {
+        
         if (other.gameObject.name.Contains ("Controller")) {
+            gemNoise.Play();
             this.setGridSpace (null);
             Destroy (this.gameObject);
         }
