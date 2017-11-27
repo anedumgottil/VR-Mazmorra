8e8bccfe70f4404aa3db8604b5ca5a5d2e716627
Andrew Podgorski <apodgo2@uic.edu>
2017-11-27 15:13:14 -0600

    MapLoader tracks state, rearranged to make it easier to understand. Also added TileGridSpaceConfigLoader

--- Technodungeon/Assets/Scripts/BlockPrefabGenerator.cs
+++ Technodungeon/Assets/Scripts/BlockPrefabGenerator.cs
@@ -10,8 +10,12 @@ public class BlockPrefabGenerator {
 	// This class exists just so I do not have to include the UnityEditor library in our build
     // It takes a Block and generates a Prefab in the Prefabs folder. These pre-generated map tiles can then be built with the project
 
-    public static void generatePrefab(Block bl) {
+    public static void generatePrefab(Block bl, bool overwrite) {
         #if UNITY_EDITOR
+        if (!overwrite && System.IO.File.Exists("Assets/Prefabs/Blocks/" + Block.PARENT_BLOCK_NAME_PREFIX + bl.getID () + ".prefab")) {
+            return;//skip an overwrite
+        }
+        //Should we be using the ReplacePrefabOptions.ReplaceNameBased argument to CreatePrefab here, or default like we currently are? especially considering an overwrite? 
         PrefabUtility.CreatePrefab ("Assets/Prefabs/Blocks/" + Block.PARENT_BLOCK_NAME_PREFIX + bl.getID () + ".prefab", bl.getGameObj ());
         #endif
         /*        //color the blox
