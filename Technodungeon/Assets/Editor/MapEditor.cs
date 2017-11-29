1fcab4092d9cf3e2f07da47cd4339fb1f049ba50
slobanwala1 <slobanwala@gmail.com>
2017-11-29 10:02:49 -0600

    Generated map at runtime for gridview

---
+++ Technodungeon/Assets/Editor/MapEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(GridGenerator))]
+public class MapEditor : Editor {
+
+	public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        GridGenerator grid = target as GridGenerator;
+
+        grid.GenerateMap();
+    }
+}
--- Technodungeon/Assets/Scripts/GridGenerator.cs
+++ Technodungeon/Assets/Scripts/GridGenerator.cs
@@ -14,12 +14,21 @@ public class GridGenerator : MonoBehaviour {
     }
     public void GenerateMap()
     {
+        string holderName = "Generated Map";
+        if(GameObject.Find(holderName))
+        {
+            DestroyImmediate(GameObject.Find(holderName).gameObject);
+        }
+
+        Transform mapHolder = new GameObject(holderName).transform;
         for(int x = 0; x < mapSize.x; x++)
         {
             for(int y = 0; y < mapSize.y; y++)
             {
-                Vector3 tilePosition = new Vector3(-mapSize.x/2 + 0.5f + x, -mapSize.y/2 + 0.5f + y);
+                Vector3 tilePosition = new Vector3(-mapSize.x/2 + 0.5f + x, 0, -mapSize.y/2 + 0.5f + y);
                 Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
+                newTile.localScale = Vector3.one * (1 - outlinePercent);
+                newTile.parent = mapHolder;
             }
         }
     }
