7147041845865376cfccd9f42f7c97e9922adf10
slobanwala1 <slobanwala@gmail.com>
2017-11-29 12:00:58 -0600

    more stuff for grid

--- Technodungeon/Assets/Scripts/GridGenerator.cs
+++ Technodungeon/Assets/Scripts/GridGenerator.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class GridGenerator : MonoBehaviour {
     public Transform tilePrefab;
+    public Transform obstaclePrefab;
     public Vector2 mapSize;
     [Range(0, 1)]
     public float outlinePercent;
 
+    List<Coord> allTileCoords;
+    Queue<Coord> shuffledTileCoords;
+    public int seed = 10;
     private void Start()
     {
         GenerateMap();
     }
     public void GenerateMap()
     {
-        string holderName = "Generated Map";
+        allTileCoords = new List<Coord>();
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                allTileCoords.Add(new Coord(x, y));
+            }
+        }
+        shuffledTileCoords = new Queue<Coord>(Utility.ShuffleArray(allTileCoords.ToArray(), seed));
+                string holderName = "Generated Map";
         if(GameObject.Find(holderName))
         {
             DestroyImmediate(GameObject.Find(holderName).gameObject);
@@ -25,11 +38,40 @@ public class GridGenerator : MonoBehaviour {
         {
             for(int y = 0; y < mapSize.y; y++)
             {
-                Vector3 tilePosition = new Vector3(-mapSize.x/2 + 0.5f + x, 0, -mapSize.y/2 + 0.5f + y);
+                Vector3 tilePosition = CoordToPosition(x, y);
                 Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
                 newTile.localScale = Vector3.one * (1 - outlinePercent);
                 newTile.parent = mapHolder;
             }
+        }
+        int obstacleCount = 10;
+        for(int i = 0; i < obstacleCount; i++)
+        {
+            Coord randomCoord = GetRandomCoord();
+            Vector3 obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
+            Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition, Quaternion.identity) as Transform;
+        }
+    }
+
+    Vector3 CoordToPosition(int x, int y)
+    {
+        return new Vector3(-mapSize.x * 2 + 0.5f + x, 0, -mapSize.y * 2 + 0.5f + y);
+    }
+    public Coord GetRandomCoord()
+    {
+        Coord randomCoord = shuffledTileCoords.Dequeue();
+        shuffledTileCoords.Enqueue(randomCoord);
+        return randomCoord;
+    }
+    public struct Coord
+    {
+        public int x;
+        public int y;
+
+        public Coord(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
         }
     }
 }
