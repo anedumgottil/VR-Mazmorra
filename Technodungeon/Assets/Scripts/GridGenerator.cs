97d12f05140df76f365c66651ea2a3877ac2f74b
slobanwala1 <slobanwala@gmail.com>
2017-11-29 09:36:43 -0600

    Pieces for enemy gridview

---
+++ Technodungeon/Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour {
+    public Transform tilePrefab;
+    public Vector2 mapSize;
+    [Range(0, 1)]
+    public float outlinePercent;
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+    public void GenerateMap()
+    {
+        for(int x = 0; x < mapSize.x; x++)
+        {
+            for(int y = 0; y < mapSize.y; y++)
+            {
+                Vector3 tilePosition = new Vector3(-mapSize.x/2 + 0.5f + x, -mapSize.y/2 + 0.5f + y);
+                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
+            }
+        }
+    }
+}
