49e9439e1846a266a46b1559b1c8a75113be8d39
Andrew Podgorski <apodgo2@uic.edu>
2017-11-30 13:18:57 -0600

    Created Reactor Room. Added StationaryEntity classes for Reactor and Coolant Line.

---
+++ Technodungeon/Assets/Scripts/CoolantLine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolantLine : StationaryEntity {
+
+    public CoolantLine(GridSpace gs) : base(gs) {
+        //fill this out
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Technodungeon/Assets/Scripts/Reactor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reactor : StationaryEntity {
+
+    public Reactor(GridSpace gs) : base(gs) {
+        //fill this out
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
