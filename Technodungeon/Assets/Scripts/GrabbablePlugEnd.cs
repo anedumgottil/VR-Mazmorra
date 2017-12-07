e71b3dd334f777341e592198d474a0fcb71b6ada
Andrew Podgorski <apodgo2@uic.edu>
2017-12-06 20:49:55 -0600

    Created particle effect systems and noises for Reactor critical state

---
+++ Technodungeon/Assets/Scripts/GrabbablePlugEnd.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class GrabbablePlugEnd : VRTK_InteractableObject {
+
+    public Reactor reactorScript = null;
+    public GameObject plugParticleEffect = null;
+
+    void Start() {
+        if (reactorScript == null) {
+            Debug.LogError ("Grabbable Plug End not given a ReactorScript!");
+        }
+        if (plugParticleEffect == null) {
+            Debug.LogError ("Grabbable Plug End not given a plugParticleEffect!");
+        }
+    }
+
+    void OnJointBreak() {
+        reactorScript.goCritical ();
+        plugParticleEffect.SetActive (true);
+    }
+}
--- Technodungeon/Assets/Scripts/Reactor.cs
+++ Technodungeon/Assets/Scripts/Reactor.cs
@@ -4,17 +4,67 @@ using UnityEngine;
 
 public class Reactor : StationaryEntity {
 
+    public GameObject grabbablePlugEnd = null;
+    public Renderer coreRenderer = null;
+    public Material criticalMaterial = null;
+
+    public GameObject ventParticles = null;
+    public GameObject coreParticles = null;
+    public float coreParticlesDuration = 1f;
+
+    private float lastReactorBurstTime = 0.0f;
+    private float nextReactorBurstWaitTime = 3f;
+    private bool isCriticalState = false;
+
     public Reactor(GridSpace gs) : base(gs) {
-        //fill this out
+    }
+
+    void Update () {
+        if (isCriticalState && alive) {
+            if (lastReactorBurstTime + nextReactorBurstWaitTime <= Time.time && !coreParticles.activeSelf) {
+                lastReactorBurstTime = Time.time;
+                nextReactorBurstWaitTime = (float)Random.Range (1, 6);
+
+                //cause reactor burst effect
+                coreParticles.SetActive (true);
+            } else if ((lastReactorBurstTime + coreParticlesDuration) <= Time.time && coreParticles.activeSelf) {
+                //stop reactor burst effect
+                coreParticles.SetActive (false);
+            }
+        }
     }
 
 	// Use this for initialization
 	void Start () {
-		
+        alive = false; 
+        if (grabbablePlugEnd == null) {
+            Debug.LogError ("Reactor: Was not given Grabbable Plug End! Cannot function!");
+            return;
+        }
+        if (coreRenderer == null) {
+            Debug.LogError ("Reactor: Was not given core Renderer! Cannot function!");
+            return;
+        }
+        if (ventParticles == null) {
+            Debug.LogError ("Reactor: Was not given ventParticles! Cannot function!");
+            return;
+        }
+        if (coreParticles == null) {
+            Debug.LogError ("Reactor: Was not given coreParticles! Cannot function!");
+            return;
+        }
+        alive = true;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void goCritical() {
+        isCriticalState = true;
+        if (criticalMaterial != null) {
+            coreRenderer.material = criticalMaterial;
+        }
+        ventParticles.SetActive (true);
+    }
+
+    public bool isCritical() {
+        return isCriticalState;
+    }
 }
